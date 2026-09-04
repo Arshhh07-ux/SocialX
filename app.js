8231b063c5baffe1c9998695d2b33ee58c8b81d8
Arshhh07-ux <priyankamishra010709@gmail.com>
2026-09-04 07:29:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+alert("Social X JavaScript Loaded");
 const CLOUDINARY_CLOUD_NAME = "Wahohh6w";
 const CLOUDINARY_UPLOAD_PRESET = "socialx";
 /* =====================================================
